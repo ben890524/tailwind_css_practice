a4aab3e7de8ad26adc75d51d058b65728a39e22b
Ben <ben10723021@gmail.com>
2022-09-04 01:41:11 +0800

    add google search - google icon

--- src/views/Google.vue
+++ src/views/Google.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="p-0 m-0 w-full h-screen">
     <google-navbar />
+    <google-search-block />
   </div>
 </template>
 
 <script setup lang="ts">
 import GoogleNavbar from '@/components/GoogleNavbar.vue';
+import GoogleSearchBlock from '@/components/GoogleSearchBlock.vue';
 </script>
 <style scoped lang="postcss">
 </style>
